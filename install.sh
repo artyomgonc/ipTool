3cfe56c314c4aca61944d6d66cdb2e935c8319b8
artgonatiacool <45709942+artgonatiacool@users.noreply.github.com>
2019-01-17 16:44:39 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash
+#to start input "bash install.sh"
+clear
+sudo echo starting...
+cp ip.pl ../ip.pl
+cd ..
+chmod +x ip.pl
+sudo apt-get install -y cpan
+sudo apt-get install -y liblocal-lib-perl
+sudo apt-get install -y libjson-perl
+sudo cpan install -y YAML
+sudo cpan install -y WWW::Mechanize
+sudo echo 'alias aboutip="perl ip.pl"' >> ~/.bashrc
+. ~/.bashrc
+echo  
+echo  
+echo  Done! To start programm print: aboutip [ip or domain]
+echo
+echo 
+rm -r ./ipTool
+exec bash
+
---
+++ ip.pl
@@ -0,0 +1,50 @@
+
+use Socket;
+use Term::ANSIColor;
+use WWW::Mechanize;
+use JSON;
+  
+ 
+print color 'bold bright_green';
+ 
+print q{
+
+
+------------------------------------
+	IP Geolocation Tool  
+------------------------------------
+
+
+};
+ 
+print color 'reset';
+@iphost=$ARGV[0];
+my @ip = inet_ntoa(scalar gethostbyname("@iphost")or die "!IP or Host invalid or you did not input it!\n\n\n");
+my @hn = scalar gethostbyaddr(inet_aton(@ip),AF_INET);
+ 
+my $GET=WWW::Mechanize->new();
+    $GET->get("http://ip-api.com/json/@ip");
+    my $json = $GET->content();
+ 
+ 
+my $info = decode_json($json);
+
+print color 'bold bright_green';
+
+print "        IP: ", $info->{'query'}, "\n";
+print "------------------------------------\n"; 
+print "  [+] Organization: ", $info->{'as'}, "\n";
+print "      Provider: ", $info->{'isp'}, "\n";
+print "\n";
+print "  [+] Country: ", $info->{'country'}," (", $info->{'countryCode'},")", "\n";
+print "      Region: ", $info->{'regionName'}, " (" , $info->{'region'},")", "\n";
+print "      City: ", $info->{'city'}, "\n";
+print "\n";
+print "  [+] Geolocation: ", "Latitude: " , $info->{'lat'}, "\n";
+print "                   ", "Longitude: ", $info->{'lon'}, "\n";
+print "\n";
+print "  [+] Timezone: ", $info->{'timezone'}, "\n";
+print "\n";
+print "\n";
+print "\n";
+print color 'reset';
