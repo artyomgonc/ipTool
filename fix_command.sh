859637a50a5c9215a73fa6d9a47e54aba8aef93c
artgonatiacool <45709942+artgonatiacool@users.noreply.github.com>
2019-01-18 18:43:31 +0300

    Update fix_command.sh

--- fix_command.sh
+++ fix_command.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo echo 'alias aboutip="perl ip.pl"' >> ~/.bashrc
 . ~/.bashrc
-RED='\033[1;33m'
+COL='\033[1;33m'
 NC='\033[0m'
 echo
-echo -e "${RED}Fixed!${NC}"
+echo -e "${COL}Fixed!${NC}"
 exec bash
